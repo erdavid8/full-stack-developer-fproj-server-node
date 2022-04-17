783f1ec3158ef20a0d157d62ab07428bbe6740f5
Edgar <edgaraland@gmail.com>
2022-04-16 23:57:27 -0400

    update pet-controller.js

--- controllers/pet-controller.js
+++ controllers/pet-controller.js
@@ -1,15 +1,22 @@
 import pets from "./users/pet.js";
+import users from "./users/users.js";
 
 const petController = (app) => {
 //  app.post('/api/pet', createProfile);
-    app.get('/api/pet/:uid', findAllPet);
-//  app.get('/api/pet', findAllProfile);
+    app.get('/api/pet/:uid', findPetById);
+    app.get('/api/pet', findAllPet);
 //  app.delete('/api/pet/:uid', deleteProfile);
 //  app.put('/api/pet/:uid', updateProfile);
 }
 
 const findAllPet = (req, res) => {
     res.json(pets);
+}
+
+const findPetById = (req, res) => {
+    const petId = req.params.uid;
+    const pet = users.find(u => u._id === petId);
+    res.json(pet);
 }
 
 export default petController;
