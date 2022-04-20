875872cd956be91af092fc3abecfcc92b1d9db2b
Edgar <edgaraland@gmail.com>
2022-04-20 02:43:56 -0400

    updated node

--- controllers/dbase/users/user-dao.js
+++ controllers/dbase/users/user-dao.js
@@ -5,7 +5,11 @@ const findProfileById = (id) => userModel.findById(id);
 const findProfileByEmail = (email) => userModel.findOne({email});
 const createProfile = (user) => userModel.insertMany(user);
 const deleteProfile = (id) => userModel.deleteOne({_id: id})
-const addlikedItem = (id, item) => userModel.updateOne({_id: id}, {$push:{$each: [item], $position: 0}})
+const addlikedItem = (id, item) => userModel.updateOne({_id: id}, {
+    $push:{
+        likedItems: {
+            $each: [item]
+        }, $position: 0}})
 
 export default {findAllProfile, findProfileById, findProfileByEmail,
     createProfile, deleteProfile, addlikedItem};
