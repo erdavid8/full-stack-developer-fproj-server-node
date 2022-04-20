6bbc07c0d1bfa62c6a8b01b787824d9cecbd6aeb
Edgar <edgaraland@gmail.com>
2022-04-20 17:36:43 -0400

    updated Node implementation

--- controllers/dbase/users/user-model.js
+++ controllers/dbase/users/user-model.js
@@ -3,8 +3,8 @@ import userAdminSchema from "./user-admin-schema.js";
 import userBuyerSchema from "./user-buyer-schema.js";
 import userSellerSchema from "./user-seller-schema.js";
 
-const userAdminModel = mongoose.model('UsersModel', userAdminSchema);
-const userBuyerModel = mongoose.model('UsersModel', userBuyerSchema);
-const userSellerModel = mongoose.model('UsersModel', userSellerSchema);
+const userAdminModel = mongoose.model('UsersAdminModel', userAdminSchema);
+const userBuyerModel = mongoose.model('UsersBuyerModel', userBuyerSchema);
+const userSellerModel = mongoose.model('UsersSellerModel', userSellerSchema);
 
 export default {userAdminModel, userBuyerModel, userSellerModel};
