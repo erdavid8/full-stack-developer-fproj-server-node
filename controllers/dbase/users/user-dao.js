5fbefe5de90bee7d502e67a3d8471c42596e6f91
Edgar <edgaraland@gmail.com>
2022-04-20 01:53:00 -0400

    updated node

--- controllers/dbase/users/user-dao.js
+++ controllers/dbase/users/user-dao.js
@@ -3,7 +3,7 @@ import userModel from "./user-model.js";
 const findAllProfile = () => userModel.find();
 const findProfileById = (id) => userModel.findById(id);
 const findProfileByEmail = (email) => userModel.findOne({email});
-const createProfile = (user) => userModel.create(user);
+const createProfile = (user) => userModel.insertMany(user);
 const deleteProfile = (id) => userModel.deleteOne({_id: id})
 
 export default {findAllProfile, findProfileById, findProfileByEmail,
