c4aa9b3bbf69a0e125d71e08776f12257de8de65
Edgar <edgaraland@gmail.com>
2022-04-20 02:29:14 -0400

    updated node

--- controllers/dbase/users/user-dao.js
+++ controllers/dbase/users/user-dao.js
@@ -5,6 +5,7 @@ const findProfileById = (id) => userModel.findById(id);
 const findProfileByEmail = (email) => userModel.findOne({email});
 const createProfile = (user) => userModel.insertMany(user);
 const deleteProfile = (id) => userModel.deleteOne({_id: id})
+const addlikedItem = (id, item) => userModel.updateOne({_id: id}, {$push:{$each: [item], $position: 0}})
 
 export default {findAllProfile, findProfileById, findProfileByEmail,
-    createProfile, deleteProfile};
+    createProfile, deleteProfile, addlikedItem};
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,34 +7,16 @@ const userController = (app) => {
     app.get('/api/user/email/:email', findProfileByEmail);
     app.get('/api/user', findAllProfile);
     app.delete('/api/user/:uid', deleteProfile);
-    //app.put('/api/user/:uid', updateProfile);
+    app.put('/api/user/:uid', addlikedItem);
 }
 
 
 // create new profile(somewhat same as signup)
 const createProfile = async (req, res) => {
     const user = req.body;
-    console.log(user);
-    console.log("iam here");
-/*  newUser._id = (new Date()).getTime() + '';
-    newUser.name = "Gilbert Repato";
-    newUser.email = "gilbert@yahoo.com";
-    newUser.password = "987654";
-    newUser.phone = "(978)-9876543";
-    newUser.type = "seller";
-    newUser.likedItems = ["123", "234", "789"];
-    newUser.postedItems = ["234"];
-    newUser.adoptedItems = ["789", "123"];
-    newUser.following = ["123", "567"];
-    console.log(newUser)
-    users.push(newUser);
-*/
     const insertUser = await userDao.createProfile(user);
-    console.log(insertUser);
-
     res.json(insertUser);
 }
-
 
 // find user using ID
 const findProfileById = async (req, res) => {
@@ -42,22 +24,21 @@ const findProfileById = async (req, res) => {
     const user = await userDao.findProfileById(userId);
 
     if (user) {
-        res.json(user);                                     // user is in database
+        res.json(user);                                             // user is in database
     } else {
-        res.sendStatus(404);                                // user not found
+        res.sendStatus(404);                                        // user not found
     }
 }
 
 // find user by email
 const findProfileByEmail = async (req, res) => {
     const userEmail = req.params['email'];
-    // const user = users.find(u => u.email === userEmail);
     const user = await userDao.findProfileByEmail(userEmail);
 
     if (user) {
-        res.json(user);                                     // user is in database
+        res.json(user);                                             // user is in database
     } else {
-        res.sendStatus(404);                                // user not found
+        res.sendStatus(404);                                        // user not found
     }
 }
 
@@ -71,9 +52,6 @@ const findAllProfile = async (req, res) => {
 // delete user profile
 const deleteProfile = async (req, res) => {
     const userId = req.params.uid;
-//    const usersIndex = users.findIndex(u => u._id === userId);          // find index of user
-//    users.splice(usersIndex, 1)                               // delete user
-
     const userDel = await userDao.deleteProfile(userId);
 
     res.json(userDel);
@@ -95,4 +73,12 @@ const updateProfile = (req, res) => {
     res.sendStatus(200);
 }
 */
+
+const addlikedItem = async (req, res) => {
+    const item = req.body
+    const userID = req.params.uid;
+    const status = await userDao.addlikedItem(userID, item);
+    res.json(status);
+}
+
 export default userController;
