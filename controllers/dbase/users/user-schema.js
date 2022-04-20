7db0fb6ec097d591bad4ba1d1f1f901611861bab
Edgar <edgaraland@gmail.com>
2022-04-20 01:12:27 -0400

    updated node

--- controllers/dbase/users/user-schema.js
+++ controllers/dbase/users/user-schema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    email: {type: String, required: true},
+    name: String,
+    email: String,
     password: String,
     phone: String,
     type: String,
