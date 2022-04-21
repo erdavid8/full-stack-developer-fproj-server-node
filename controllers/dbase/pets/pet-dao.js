808fcdffb2a765effc8e4fc9414e970bae5e91b2
Edgar <edgaraland@gmail.com>
2022-04-21 03:30:01 -0400

    updated Node implementation

--- controllers/dbase/pets/pet-dao.js
+++ controllers/dbase/pets/pet-dao.js
@@ -4,7 +4,7 @@ import petModel from "./pet-model.js";
 
 const findAllPets = () => petModel.find();
 const findPetByID = (id) => petModel.findById(id);
-const findPetByName = (name) => petModel.findOne(name);
+const findPetByName = (name) => petModel.findOne({name:{$eq: name}});
 const createPetProfile = (pet) => petModel.insertMany(pet);
 const deletePet = (id) => petModel.deleteOne({_id: id})
 
