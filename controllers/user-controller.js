62d1e3de76157958993713b21293d3098dc6ef30
Edgar <edgaraland@gmail.com>
2022-04-20 22:21:13 -0400

    updated Node implementation

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -183,7 +183,7 @@ const updateProfile = (req, res) => {
 
 const addlikedItem = async (req, res) => {
     //const item = req.body
-    const updateItem = req.headers.key;
+    const updateItem = req.headers;
     console.log(updateItem);
     const userID = req.params.uid;
     console.log(userID);
