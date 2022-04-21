3ff8bc8d6858c10aa1927481ced308846efa4d56
Edgar <edgaraland@gmail.com>
2022-04-21 02:04:26 -0400

    updated Node implementation

--- controllers/pet-controller.js
+++ controllers/pet-controller.js
@@ -38,7 +38,7 @@ const findPetById = async (req, res) => {
 
 // find pet by name
 const findPetByName = async (req, res) => {
-    const petName = req.params['name'];
+    const petName = "Liberty";
     console.log(petName);
     console.log("IAM HERE")
     const pet = await petDao.findPetByName(petName);
